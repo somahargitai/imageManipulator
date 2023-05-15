2092c5901afa035dbfcef81cafc5582bc7fc77da
Soma Hargitai <hargitai.soma@gmail.com>
2023-05-15 18:40:40 +0200

    image compress script

---
+++ src/compressImage/contants.js
@@ -0,0 +1,13 @@
+const contants = {
+  ROOT: "../image_container_folder/weboptimization_input",
+  OUTPUT: "../image_container_folder/weboptimization_output",
+  TEMP: "../image_container_folder/weboptimization_temp",
+  RESIZE_IMAGE_WIDTH: 1000, // IN PIXELS
+  JIMP_QUALITY: 70,
+  MAX_IMAGE_SIZE: 350000, // IN BYTES 350 KB
+  RESIZE_HD_IMAGE_WIDTH: 400,
+  MEDIAN_HD_IMAGE_WIDTH: 700,
+  MAX_HD_IMAGE_SIZE: 650000,
+};
+
+module.exports = contants;
